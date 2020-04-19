681958f9bbb1b752315ccd52aca972b821b8bac8
vbirukov <vbirukov@gmail.com>
2020-04-19 23:37:13 +0300

    new homework

--- StringsHomweork/Source.cpp
+++ StringsHomweork/Source.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 
-void printEvenOdd(int startFrom, int limit) {
-	int i = startFrom ? 2 : 1;
-	while (i <= limit) {
-		std::cout << i << '\n';
-		i += 2;
-	}
-}
-
 int main() {
+	int today = 20;
+	const int size = 15;
+	int Matrix[size][size];
 
-	int limit = 100;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			Matrix[i][j] = i + j;
+			std::cout << Matrix[i][j] << ' ';
+		}
+		std::cout << '\n';
+	}
 
-	for (int i = 0; i <= limit; i++) {
-		if (i%2 == 0) {
-			std::cout << i << '\n';
+	std::cout << "============" << '\n';
+	
+
+	for (int i = 0; i < size; i++) {
+		int sum = 0;
+		for (int j = 0; j < size; j++) {
+			sum += Matrix[i][j];
+		}
+		if (sum % today == 0) {
+			std::cout << sum << '\n';
 		}
 	}
-
-	printEvenOdd(1, 20);
 }
